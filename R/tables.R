c182d1ee6495ea3aeb0fc369ba19d9ab83048a09
NewGraphEnvironment <al@newgraphenvironment.com>
2022-05-02 10:43:15 -0700

    minor text edits (remove coments) in fpr_table_cv_detailed

--- R/tables.R
+++ R/tables.R
@@ -323,9 +323,10 @@ fpr_table_cv_summary_memo <- function(dat = pscis_phase2,
                                       font = 11,
                                       col_filter = pscis_crossing_id){
   fpr_table_cv_detailed(dat = dat %>% filter({{ col_filter }} == site)) %>%
-    kable(caption = paste0('Summary of fish passage assessment for PSCIS crossing ', site, '.'), booktabs = T) %>%    #
-    # kableExtra::add_footnote(label = paste0('Comments: ', dat %>% filter({{ col_filter }} == site) %>%
-    #                                           pull(assessment_comment)), notation = 'none') %>% #this grabs the comments out
+    kable(caption = paste0('Summary of fish passage assessment for PSCIS crossing ', site, '.'), booktabs = T) %>%
+    #this grabs the comments out
+    kableExtra::add_footnote(label = paste0('Comments: ', dat %>% filter({{ col_filter }} == site) %>%
+                                              pull(assessment_comment)), notation = 'none') %>%
     kableExtra::add_footnote(label = paste0('Photos: From top left clockwise: Road/Site Card, Barrel, Outlet, Downstream, Upstream, Inlet.',
                                             paste0('![](data/photos/', site_photo_id, '/crossing_all.JPG)')), notation = 'none') %>%
     kableExtra::kable_styling(c("condensed"), full_width = T, font_size = font)
