8f20a9e25d9e0188a2eff1497c7315881b8aa5fb
almac2022 <al@newgraphenvironment.com>
2022-12-21 10:14:23 -0800

    fix error in documentation

--- R/import.R
+++ R/import.R
@@ -8,6 +8,8 @@
 #' of location sheet (`step_1_ref_and_loc_info') is populated to row 1500 out of box. Note that this !is.na for col 2 results in some
 #' trimming of a couple events on the admin_only_pick_lists.csv
 #' @param dat Dataframe
+#' @param row_empty_remove Logical on whether to remove empty dataframe rows until the first complete one is found in
+#' order to select as rownames.  Needs to be turned to TRUE for `fpr_import_hab_con` to work.
 #'
 #' @description Select column names based on first row with inputs all across.
 #' Trim up the dataframe based on whether there are events in the first column.
@@ -18,10 +20,17 @@
 #' @examples
 fpr_sheet_trim <- function(dat,
                            col_filter_na = FALSE,
-                           col_filter_na_num = 2) {
+                           col_filter_na_num = 2,
+                           row_empty_remove = F) {
   dat2 <- dat %>%
-    dplyr::select(1:ncol(dat)) %>% ##get rid of the extra columns because there are remnants way down low I believe
-    janitor::row_to_names(which.max(stats::complete.cases(.))) %>%
+    dplyr::select(1:ncol(dat)) ##get rid of the extra columns because there are remnants way down low I believe
+
+  if(row_empty_remove){
+    dat2 <- dat2 %>%
+    janitor::row_to_names(which.max(stats::complete.cases(.)))
+  }
+
+  dat2 <- dat2 %>%
     janitor::clean_names() %>%
     janitor::remove_empty(., which = "rows") %>%
     # remove rows that don't have a value in the first column
@@ -36,39 +45,44 @@ fpr_sheet_trim <- function(dat,
 
 #' Import the pscis template
 #'
+#' @param dir_root String indicating name of directory to look for pscis submission file
 #' @param workbook_name string value for name of worksheet
-#' @description Import the pscis template.  This is a helper for fpr_import_all
+#'
+#' @description Import the pscis template. Assigns the column types based on the fpr_xref_pscis data object.
+#' Extracts a time_start and a date_time_start of the survey from the comments if it is sritten in 24hr format after the last period of the comments column.
+#' Assigns the camera tot hte first person listed in the crew memebers column. Creates a site_id as either the PSCIS id or the my crossing reference
+#'  This is a helper for fpr_import_pscis_all.
 #' @return A dataframe
 #' @export
 #'
 #' @examples
-fpr_import_pscis <- function(workbook_name = 'pscis_phase1.xlsm'){ ##new template.  could change file back to .xls
+fpr_import_pscis <- function(workbook_name = 'pscis_phase1.xlsm', ##new template.  could change file back to .xls
+                             dir_root = 'data'){
   sig_fig0 <- c('length_or_width_meters')
   sig_fig1 <- c('culvert_slope_percent', 'stream_width_ratio')
   sig_fig2 <- c('outlet_drop_meters')
-  readxl::read_excel(path = paste0(getwd(),"/data/", workbook_name),
-                     sheet = 'PSCIS Assessment Worksheet') %>%
-    # purrr::set_names(janitor::make_clean_names(names(.))) %>%
+  readxl::read_excel(path = paste0(dir_root, "/", workbook_name),
+                     sheet = 'PSCIS Assessment Worksheet',
+                     skip = 3,
+                     .name_repair = janitor::make_clean_names,
+                     col_types = fpr_xref_pscis %>% filter(type_on_import == T) %>% pull(type_readxl)) %>%
     fpr_sheet_trim() %>% ##recently added function above and pulled the altools package as it was a week link
     tibble::rowid_to_column() %>%
     dplyr::rename(date = date_of_assessment_yyyy_mm_dd) %>%
     dplyr::filter(!is.na(date)) %>%
-    readr::type_convert() %>%  ##guess the type!!
     dplyr:: mutate(date = janitor::excel_numeric_to_date(as.numeric(date)),
                    source = workbook_name,
                    dplyr::across(dplyr::all_of(sig_fig0), round, 0),
                    dplyr::across(dplyr::all_of(sig_fig1), round, 1),
                    dplyr::across(dplyr::all_of(sig_fig2), round, 2),
                    rowid = rowid + 4,
-                   pscis_crossing_id = as.numeric(pscis_crossing_id),
-                   my_crossing_reference = as.numeric(my_crossing_reference),
                    aggregated_crossings_id = dplyr::case_when(!is.na(pscis_crossing_id) ~ pscis_crossing_id,
                                                               my_crossing_reference > 200000000 ~ my_crossing_reference,  ##date based id's are greater than this number
                                                               T ~ my_crossing_reference + 1000000000),
                    time_start = stringr::str_squish(stringr::str_extract(assessment_comment, "[^.|^?]*$")), #assumes the time is positioned after last "."
                    date_time_start = lubridate::ymd_hm(paste0(date, ' ', time_start)),
                    camera_id = stringr::word(crew_members, 1), ##identify who had the camera as this is the first initials in the crew_members
-                   site_id = case_when(!is.na(pscis_crossing_id) ~ pscis_crossing_id,
+                   site_id = dplyr::case_when(!is.na(pscis_crossing_id) ~ pscis_crossing_id,
                                        T ~ my_crossing_reference)
     )
 }
@@ -101,7 +115,7 @@ fpr_import_pscis_all <- function(backup = TRUE,
   wkbs_paths <- fpr_pscis_wkb_paths()
 
   pscis_list <- wkbs_paths %>%
-    map(fpr_import_pscis) %>%
+    purrr::map(fpr_import_pscis) %>%
     purrr::set_names(nm = tools::file_path_sans_ext(wkbs_paths))
 
   if(backup){
@@ -149,7 +163,8 @@ fpr_import_hab_prior <- function(input = 'data/habitat_confirmations_priorities.
 #' https://www2.gov.bc.ca/gov/content/environment/plants-animals-ecosystems/fish/fish-and-fish-habitat-data-information/fish-data-submission/submit-fish-data#submitfish
 #' @param backup Logical whether to backup all sheets as csvs or not. Defaults to true
 #' @param path_backup String indicating directory to create and backup to.  Defaults to 'data/backup/'
-#' @param ... Not used.  For passing true or false to col_filter_na and col_filter_na_num from \link{fpr_sheet_trim}
+#' @param ... Not used.  For passing arguments from \link{fpr_sheet_trim}.  For \link{fpr_import_hab_con} we pass the switch row_empty_remove = T because we have not yet assigned column types. We can also pass
+#' "true" or "false" to col_filter_na and col_filter_na_num to change which column we use to drop off rows
 #' @seealso \link{fpr_sheet_trim}
 #'
 #' @return
