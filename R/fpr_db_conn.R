acf8b1600eaf9d5196b4a0f89282fb327ebccdb1
almac2022 <al@newgraphenvironment.com>
2024-01-12 17:59:16 -0800

    add new functions

--- R/fpr_db_conn.R
+++ R/fpr_db_conn.R
@@ -1,6 +1,6 @@
 #' Connect to postgres database
 #'
-#' Connect to pg database using environmental variables
+#' Connect to pg database using environmental variables.  Ass
 #'
 #' @param db_var Quoted string value of database name.  Defaults to `Sys.getenv('PG_DB_SHARE')`
 #' @param host_var Quoted string value of host name.  Defaults to `Sys.getenv('PG_HOST_SHARE')`
@@ -8,10 +8,12 @@
 #' @param user_var Quoted string value of user name.  Defaults to `Sys.getenv('PG_USER_SHARE')`
 #' @param password_var QQuoted string value of password.  Defaults to `Sys.getenv('PG_PASS_SHARE')`
 #'
+#' @family database postgres
+#'
 #' @return
 #' @export
 #'
-#' @examples \dontrun{fpr_db_conn()}
+#' @examples \dontrun{conn <- fpr_db_conn()}
 fpr_db_conn <- function(
     db_var = Sys.getenv('PG_DB_SHARE'),
     host_var = Sys.getenv('PG_HOST_SHARE'),
