35ca8e27d7a801b3e744ab74f4fc1224cb723db1
almac2022 <al@newgraphenvironment.com>
2024-01-15 08:35:34 -0800

    add pass and fail tests

--- tests/testthat/test-fpr_db_conn.R
+++ tests/testthat/test-fpr_db_conn.R
@@ -1,8 +1,19 @@
-testthat::test_that("dummy test", {
+# this on allows some writing so
+# testthat::test_that("connection is successful", {
+#   testthat::expect_equal(
+#     class(fpr_db_conn())[1],
+#     "PqConnection"
+#   )
+# })
+
+testthat::test_that("connection is successful", {
   testthat::expect_equal(
-    5,
-    5
+    class(fpr_db_conn(user_var = Sys.getenv('PG_USER_SHARE_VW'),
+                      password_var = Sys.getenv('PG_PASS_SHARE_VW')))[1],
+    "PqConnection"
   )
 })
 
-
+test_that("connection unsuccessful fails", {
+  expect_error(fpr_db_conn(db_var = 'wrong'))
+})
