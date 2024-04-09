e58d289f0168ec44777d6a0144a15bf13eaab552
almac2022 <al@newgraphenvironment.com>
2024-04-09 09:38:30 -0700

    close #57

--- R/fpr_xref_pscis_modelled.R
+++ R/fpr_xref_pscis_modelled.R
@@ -5,7 +5,8 @@
 #' selects specific columns, cleans the column names, drops unnecessary columns and geometry data, and converts all
 #' remaining columns to numeric. The resulting data frame serves as a cross-reference for PSCIS modeled data.
 #'
-#' @param project_id A character string (quoted) specifying the project ID (or list of project IDS separated by commas)
+#' @param project_id A character string (quoted) specifying the project ID (or list of project IDS separated by commas).
+#' The project_id is detailed as the `FUNDING_PROJECT_NUMBER` in the BC Data Catalougue
 #' to filter the data on. Default is NULL.
 #' @return A data frame with cleaned names and numeric columns, serving as a cross-reference for PSCIS modeled data.
 #' @importFrom bcdata bcdc_query_geodata
@@ -19,12 +20,24 @@
 #' @export
 fpr_xref_pscis_modelled <- function(
     project_id = NULL){
-  bcdata::bcdc_query_geodata("7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881") %>%
+ x <- bcdata::bcdc_query_geodata("7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881") %>%
     dplyr::filter(FUNDING_PROJECT_NUMBER %in% c(project_id)) %>%
     dplyr::select(EXTERNAL_CROSSING_REFERENCE, STREAM_CROSSING_ID) %>%
     bcdata::collect() %>%
-    janitor::clean_names() %>%
+    janitor::clean_names()
+
+
+ if (nrow(x) == 0) {
+   cli::cli_abort("`project_id` (a.k.a `FUNDING_PROJECT_NUMBER`) not found in the BC Data Catalogue. Please check the
+                  `project_id` and try again.")
+
+ }
+
+
+ if (nrow(x) > 0) {
+   x %>%
     dplyr::select(-id, -objectid) %>%
     sf::st_drop_geometry() %>%
     dplyr::mutate(dplyr::across(dplyr::everything(), as.numeric))
+ }
 }
