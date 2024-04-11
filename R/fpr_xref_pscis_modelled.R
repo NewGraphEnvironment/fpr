8b8c86951bb8ec08534ece847780ec5746bb9188
almac2022 <al@newgraphenvironment.com>
2024-04-11 14:25:01 -0700

    just line indenting

--- R/fpr_xref_pscis_modelled.R
+++ R/fpr_xref_pscis_modelled.R
@@ -20,24 +20,22 @@
 #' @export
 fpr_xref_pscis_modelled <- function(
     project_id = NULL){
- x <- bcdata::bcdc_query_geodata("7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881") %>%
+  x <- bcdata::bcdc_query_geodata("7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881") %>%
     dplyr::filter(FUNDING_PROJECT_NUMBER %in% c(project_id)) %>%
     dplyr::select(EXTERNAL_CROSSING_REFERENCE, STREAM_CROSSING_ID) %>%
     bcdata::collect() %>%
     janitor::clean_names()
 
-
- if (nrow(x) == 0) {
-   cli::cli_abort("`project_id` (a.k.a `FUNDING_PROJECT_NUMBER`) not found in the BC Data Catalogue. Please check the
+  if (nrow(x) == 0) {
+    cli::cli_abort("`project_id` (a.k.a `FUNDING_PROJECT_NUMBER`) not found in the BC Data Catalogue. Please check the
                   `project_id` and try again.")
 
- }
+  }
 
-
- if (nrow(x) > 0) {
-   x %>%
-    dplyr::select(-id, -objectid) %>%
-    sf::st_drop_geometry() %>%
-    dplyr::mutate(dplyr::across(dplyr::everything(), as.numeric))
- }
+  if (nrow(x) > 0) {
+    x %>%
+      dplyr::select(-id, -objectid) %>%
+      sf::st_drop_geometry() %>%
+      dplyr::mutate(dplyr::across(dplyr::everything(), as.numeric))
+  }
 }
