2de674a5b9f7e92e6889d4a4911768b1692522cb
almac2022 <al@newgraphenvironment.com>
2024-12-20 11:09:40 -0800

    add docs for fpr_chk_dupes and export the fuction

--- R/fpr_chk_dupes.R
+++ R/fpr_chk_dupes.R
@@ -1,3 +1,22 @@
+#' Check for Duplicate Values in a Column
+#'
+#' This function checks for duplicate values in a specified column of a dataset and provides a warning if duplicates are found.
+#'
+#' @param dat [data.frame] A data frame to be checked for duplicates.
+#' @param col_screen A column in the data frame to check for duplicates. Must be specified as a tidy evaluation argument.
+#'
+#' @details
+#' The function identifies duplicate values in the column and provides a warning with a list of the duplicate values if any are found.
+#'
+#' @importFrom chk chk_not_missing chk_data
+#' @importFrom rlang enquo as_name
+#' @importFrom dplyr filter group_by n ungroup distinct pull
+#' @importFrom cli cli_abort cli_alert_danger
+#' @importFrom glue glue_collapse
+#'
+#' @return The function does not return a value but provides a warning if duplicates are found.
+#'
+#' @export
 fpr_chk_dupes <- function(dat = NULL, col_screen = NULL) {
   # Validate inputs
   chk::chk_not_missing(dat)
@@ -9,7 +28,6 @@ fpr_chk_dupes <- function(dat = NULL, col_screen = NULL) {
   if (!col_name %in% names(dat)) {
     cli::cli_abort("{.val {col_name}} is not a valid column in the dataset.")
   }
-
 
   # Find duplicates
   dupes <- dat |>
