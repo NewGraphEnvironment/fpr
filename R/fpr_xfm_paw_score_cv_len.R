ae0cb189bfb99ef90d39a3cf699f619981fb00a1
almac2022 <al@newgraphenvironment.com>
2025-01-16 09:47:36 -0800

    bind all input columns and abract the scores and risk values

--- R/fpr_xfm_paw_score_cv_len.R
+++ R/fpr_xfm_paw_score_cv_len.R
@@ -2,10 +2,16 @@
 #'
 #' This function calculates the culvert length score for BC Provincial Stream Crossing Inventory System (PSCIS) data based on the given length or width column.
 #'
-#' @param dat A dataframe containing the PSCIS data.
-#' @param col_length_or_width_meters Column name for length or width, as a string or tidy-select syntax. Default is `length_or_width_meters`.
+#' @param dat [dataframe] A dataframe containing the PSCIS data.
+#' @param col_length_or_width_meters [character] Column name for length or width, as a string or tidy-select syntax. Default is `length_or_width_meters`.
+#' @param col_culvert_length_score [character] Column name for the culvert length score, as a string. Default is `culvert_length_score`.
+#' @param risk_low_value [numeric] A numeric value representing the lower threshold for culvert length risk. Default is `15`.
+#' @param risk_high_value [numeric] A numeric value representing the threshold for culvert length risk. Default is `30`.
+#' @param risk_low_score [numeric] A numeric value representing the low risk score. Default is `0`.
+#' @param risk_mod_score [numeric] A numeric value representing the moderate risk score. Default is `3`.
+#' @param risk_high_score [numeric] A numeric value representing the high risk score. Default is `6`.
 #'
-#' @return A dataframe with `culvert_length_score` column added or updated.
+#' @return [dataframe] A dataframe with `culvert_length_score` column added or updated.
 #'
 #' @examples
 #' dat <- data.frame(
@@ -22,13 +28,20 @@
 #' @export
 fpr_xfm_paw_score_cv_len <- function(
     dat,
-    col_length_or_width_meters = length_or_width_meters
+    col_length_or_width_meters = length_or_width_meters,
+    col_culvert_length_score = culvert_length_score,
+    risk_low_value = 15,
+    risk_high_value = 30,
+    risk_low_score = 0,
+    risk_mod_score = 3,
+    risk_high_score = 6
 ) {
   # Check input validity
   chk::chk_data(dat)
 
-  # Evaluate tidy-select column
+  # Evaluate tidy-select columns
   col_length_or_width_meters <- rlang::ensym(col_length_or_width_meters)
+  col_culvert_length_score <- rlang::ensym(col_culvert_length_score)
 
   # Check if required column exists in the dataframe
   required_cols <- as.character(col_length_or_width_meters)
@@ -39,12 +52,12 @@ fpr_xfm_paw_score_cv_len <- function(
   # Calculate culvert_length_score
   dat <- dat |>
     dplyr::mutate(
-      culvert_length_score = dplyr::case_when(
-        is.na(!!col_length_or_width_meters) ~ 0,
-        !!col_length_or_width_meters < 15 ~ 0,
-        !!col_length_or_width_meters >= 15 & !!col_length_or_width_meters < 30 ~ 3,
-        !!col_length_or_width_meters >= 30 ~ 6,
-        TRUE ~ 0
+      !!col_culvert_length_score := dplyr::case_when(
+        is.na(!!col_length_or_width_meters) ~ risk_low_score,
+        !!col_length_or_width_meters < risk_low_value ~ risk_low_score,
+        !!col_length_or_width_meters >= risk_low_value & !!col_length_or_width_meters < risk_high_value ~ risk_mod_score,
+        !!col_length_or_width_meters >= risk_high_value ~ risk_high_score,
+        TRUE ~ risk_low_score
       )
     )
 
