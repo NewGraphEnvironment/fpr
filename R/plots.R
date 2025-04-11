cb59d19429d7d0953e2623ecc66dd2733268f086
newgraph-lschick <lucy@newgraphenvironment.com>
2025-04-11 14:52:37 -0700

    Add param theme and set default to theme_bw as per https://github.com/NewGraphEnvironment/fpr/issues/113

--- R/plots.R
+++ R/plots.R
@@ -4,25 +4,24 @@
 #' @param dat Dataframe. Defaults to fish_abund.  Must contian the columns site, species_code,
 #' life_stage, density_100m2
 #' @param sit Integer. ID for the site. Defaults to my_site.
+#' @param theme Function. Theme used for the plot. Defaults to `ggplot2::theme_bw()`.
 #'
 #' @return Boxplot
 #' @export
 #'
 #' @examples
-fpr_plot_fish_box <- function(dat = fish_abund, sit = my_site){
-  dat %>%
-    filter(
-      site  == sit
-      &
+fpr_plot_fish_box <- function(dat = fish_abund, sit = my_site, theme = ggplot2::theme_bw()){
+  dat |>
+    dplyr::filter(
+      site == sit &
         species_code != 'NFC'
-    ) %>%
-    ggplot(., aes(x = location, y =density_100m2)) +
-    geom_boxplot()+
-    facet_grid(species_code ~ life_stage, scales ="fixed",
-               as.table = T)+
-    dark_theme_bw()+
-    theme(legend.position = "none", axis.title.x=element_blank()) +
-    geom_dotplot(binaxis='y', stackdir='center', dotsize=1)+
-    ylab(expression(Density ~ (fish/100 ~  m^2)))
+    ) |>
+    ggplot2::ggplot(ggplot2::aes(x = location, y = density_100m2)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::facet_grid(species_code ~ life_stage, scales = "fixed", as.table = TRUE) +
+    theme +
+    ggplot2::theme(legend.position = "none", axis.title.x = ggplot2::element_blank()) +
+    ggplot2::geom_dotplot(binaxis = 'y', stackdir = 'center', dotsize = 1) +
+    ggplot2::ylab(expression(Density ~ (fish / 100 ~ m^2)))
 }
 
