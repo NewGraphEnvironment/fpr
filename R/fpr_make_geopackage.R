d39f368e3a54139f0523e39b17fe46d49b75bcad
almac2022 <al@newgraphenvironment.com>
2025-06-03 16:10:55 -0700

    use fs::path to avoid file path slash issues and close #50 again

--- R/fpr_make_geopackage.R
+++ R/fpr_make_geopackage.R
@@ -1,4 +1,4 @@
-#' Make Geopackage from an sf Object
+#' Make Geopackage from an dataframe with UTM coordinates
 #'
 #' Creates a directory (if it doesn't exist) and writes a layer to a GeoPackage
 #' using the name of the input `dat` object as the layer name.
@@ -22,7 +22,7 @@
 #' @seealso [fpr_sp_assign_sf_from_utm()], [sf::st_write()]
 #'
 #' @importFrom sf st_transform st_write
-#' @importFrom fs dir_create
+#' @importFrom fs dir_create path
 #' @export
 #'
 #' @examples
@@ -34,26 +34,29 @@
 #' fpr_make_geopackage(
 #'   dat = dat,
 #'   col_easting = "utm_easting",
-#'   col_northing = "utm_northing",
-#'   crs_return = 4326
+#'   col_northing = "utm_northing"
 #' )
 fpr_make_geopackage <- function(dat,
                                 dir = "data/fishpass_mapping/",
                                 gpkg_name = "fishpass_mapping",
+                                crs = 4326,
                                 ...) {
   if (is.null(dat))
-    cli::cli_abort('please provide "dat" object')
+  cli::cli_abort('please provide "dat" object')
   if (!is.data.frame(dat))
     cli::cli_abort('"dat" must be a data.frame')
   chk::chk_string(dir)
   chk::chk_string(gpkg_name)
+  chk::chk_number(crs)
 
   fs::dir_create(dir)
   nm <- deparse(substitute(dat))
 
   dat |>
     fpr_sp_assign_sf_from_utm(...) |>
-    sf::st_write(paste0(dir, gpkg_name, ".gpkg"), nm, delete_layer = TRUE) |>
+    sf::st_transform(crs = crs) |>
+    sf::st_write(fs::path(dir, paste0(gpkg_name, ".gpkg")), nm, delete_layer = TRUE) |>
     invisible()
 }
+
 
--- tests/testthat/test-fpr_make_geopackage.R
+++ tests/testthat/test-fpr_make_geopackage.R
@@ -1,24 +1,24 @@
-# test_that("fpr_make_geopackage writes a .gpkg file", {
-#   test_dir <- fs::path(tempdir(), "test_gpkg_dir")
-#   if (!fs::dir_exists(test_dir)) fs::dir_create(test_dir)
-#
-  # dat <- data.frame(
-  #   utm_zone = 10,
-  #   utm_easting = c(610000, 611000),
-  #   utm_northing = c(5600000, 5601000)
-  # )
-  #
-  # fpr_make_geopackage(
-  #   dat = dat,
-  #   dir = test_dir,
-  #   gpkg_name = "test_layer",
-  #   col_easting = "utm_easting",
-  #   col_northing = "utm_northing",
-  #   crs_return = 4326
-  # )
-#
-#   file_out <- fs::path(test_dir, "test_layer.gpkg")
-#   expect_true(fs::file_exists(file_out))
-# })
+test_that("fpr_make_geopackage writes a .gpkg file", {
+  test_dir <- fs::path(tempdir(), "test_gpkg_dir")
+  if (!fs::dir_exists(test_dir)) fs::dir_create(test_dir)
+
+dat <- data.frame(
+  utm_zone = 10,
+  utm_easting = c(610000, 611000),
+  utm_northing = c(5600000, 5601000)
+)
+
+fpr_make_geopackage(
+  dat = dat,
+  dir = test_dir,
+  gpkg_name = "test_layer",
+  col_easting = "utm_easting",
+  col_northing = "utm_northing",
+  crs_return = 4326
+)
+
+  file_out <- fs::path(test_dir, "test_layer.gpkg")
+  expect_true(fs::file_exists(file_out))
+})
 
 
