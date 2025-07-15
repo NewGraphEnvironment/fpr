fe4378dc7317b3fe7a43d8674229b8c2a123a8c5
almac2022 <al@newgraphenvironment.com>
2025-07-14 17:38:54 -0700

    still closes https://github.com/NewGraphEnvironment/fpr/issues/115

--- R/fpr_table_cv_summary_memo.R
+++ R/fpr_table_cv_summary_memo.R
@@ -14,13 +14,26 @@ fpr_table_cv_summary_memo <- function(dat = pscis_phase2,
                                       site = my_site,
                                       site_photo_id = my_site,
                                       font = 11,
-                                      col_filter = pscis_crossing_id){
-  fpr_table_cv_detailed(dat = dat %>% filter({{ col_filter }} == site)) |>
-    fpr_kable(caption_text = paste0("Summary of fish passage assessment for PSCIS crossing ",
-                                    site, "."), booktabs = T, scroll = FALSE, font = font) |>
-    #this grabs the comments out
-    kableExtra::add_footnote(label = paste0('Comments: ', dat %>% filter({{ col_filter }} == site) |>
-                                              pull(assessment_comment)), notation = 'none') %>%
-    kableExtra::add_footnote(label = paste0('Photos: From top left clockwise: Road/Site Card, Barrel, Outlet, Downstream, Upstream, Inlet.',
-                                            paste0('![](data/photos/', site_photo_id, '/crossing_all.JPG)')), notation = 'none')
+                                      col_filter = pscis_crossing_id) {
+
+  dat_site <- dat |> dplyr::filter({{ col_filter }} == site)
+  comments <- dat_site |> dplyr::pull(assessment_comment)
+
+  comment_label <- paste0("Comments: ", comments)
+
+  photo_label <- "Photos: From top left clockwise: Road/Site Card, Barrel, Outlet, Downstream, Upstream, Inlet."
+  photos_inserted <- paste0("![](data/photos/", site_photo_id, "/crossing_all.JPG)")
+
+  fpr::fpr_kable(
+    fpr::fpr_table_cv_detailed(dat = dat_site),
+    caption_text = paste0("Summary of fish passage assessment for PSCIS crossing ", site, "."),
+    booktabs = TRUE,
+    scroll = FALSE,
+    font = font
+  ) |>
+    kableExtra::add_footnote(
+      label = c(comment_label, photo_label, photos_inserted),
+      notation = "none"
+    )
 }
+
